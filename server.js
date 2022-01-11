2615b87e75aa51c5766a745897c1c895249cda2e
Jwbranch4 <87912530+Jwbranch4@users.noreply.github.com>
2022-01-11 13:50:39 -0600

    created server.js to connect express

---
+++ server.js
@@ -0,0 +1,24 @@
+// import express and fs
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// use routes
+
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+// middle ware for express
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(express.json());
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`api server now on port ${PORT}`);
+});
